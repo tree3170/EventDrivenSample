74781db4fb06d716c6a295cd95eca69f37c34689
tree3170 <tree317035791@163.com>
2018-11-30 21:24:11 +0800

    增加uml及注释

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Spring事件传播机制
+
+具体参考下面图：<p>
+ApplicationEvent.uml<p>
+ApplicationListener.uml<p>
+spring事件驱动模型.jpg<p>
+
+## 参考：
+### [Spring3.2.6中事件驱动模型实现原理深入源码分析](https://blog.csdn.net/u011066648/article/details/50997222)
+### [spring事件通知机制详解](https://www.cnblogs.com/zhangxiaoguang/p/spring-notification.html)
--- src/main/java/com/tree/spring/xml/EventListenerTest.java
+++ src/main/java/com/tree/spring/xml/EventListenerTest.java
@@ -10,18 +10,19 @@ import java.util.concurrent.TimeUnit;
 /**
  * package com.tree.spring
  * description:
- * MealListener ：MealEvent                  演员
- * <p>
- * TroubleListener ：TroubleEvent         演员
- * <p>
- * AllAcceptedListener                            演员
- * <p>
- * MealEnum                                          道具
- * <p>
- * EventListenerTest
- * <p>
+ * MealListener ：MealEvent
+ * MealEnum
  * CustomizePublisher
+ * EventListenerTest
  *
+ * 事件（Event）： 用户点击按钮（Button），就会产生事件（Event）。这里就是点击事件（ClickEvent）。
+ * 事件源（EventSource）：发生事件的根源（场所）就是事件源。这里按钮组件就是事件源。
+ * 事件监听器（EventListener）：监听特定的事件并处理的程序。这里监听器就是点击事件的处理者。
+ * 事件分发者（EventDispatcher）：将发生的事件通知相对应的事件处理者的程序。这里就是通知监听器处理事件的程序。
+ *
+ * 参考：
+ * Spring3.2.6中事件驱动模型实现原理深入源码分析 ： https://blog.csdn.net/u011066648/article/details/50997222
+ * spring事件通知机制详解 ：https://www.cnblogs.com/zhangxiaoguang/p/spring-notification.html
  * @author tree
  * @date 2018-11-30 09:32
  */
